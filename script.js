284a9e7b1cae0b9be89401e286935a1369d57604
Dewrong <ysidda33@gmail.com>
2022-05-31 16:41:42 -0500

    added question and hint tabs

--- script.js
+++ script.js
@@ -1,34 +1,76 @@
 
+
+//Add a hint box that swaps between all the hints, center the hints, clarify the next button as a give up.
 //intake answers should be toLowerCase() for === match
+let questionNumber = 0;
+let specimenAnswer = "default";
 
-function displayQuestion(){
-    let question =  document.getElementsByClassName("hide-question");
-    for (let i = 0; i < question.length; i++){
-        question[i].style.display ="inline";
+
+function displayQuestion() {
+    document.getElementById("next-btn").style.display = "inherit";
+    document.getElementById("game-description").style.display = "none";
+    document.getElementById("start-btn").style.display = "none";
+    fetch('./questionList.json')
+        .then(questions => questions.json())
+        .then(data => {
+            specimenAnswer = data[questionNumber].species;
+            document.getElementById("question").innerHTML = data[questionNumber].funFact;
+            document.getElementById("hint-1").innerHTML = data[questionNumber].hints[0];
+            document.getElementById("hint-2").innerHTML = data[questionNumber].hints[1];
+            document.getElementById("hint-3").innerHTML = data[questionNumber].hints[2];
+            questionNumber++;
+        });
+    let question = document.getElementsByClassName("hide-question");
+    for (let i = 0; i < question.length; i++) {
+        question[i].style.display = "inline";
     }
-    document.getElementById("question").innerHTML = "This animal can sleep for three years.";
 }
 
-function displayHint1(){
-    let hint = document.getElementsByClassName("hide-hint-1");
-    for (let i=0; i <hint.length; i++){
-        hint[i].style.display = "inline";
+function submitButton() {
+    let userInput = document.getElementById("answer-input").value;
+    if (specimenAnswer == userInput) {
+        document.getElementById("submit-button").style.display = "none";
+        document.getElementById("validator").innerHTML = "Correct!";
     }
-    document.getElementById("hint-1").innerHTML = "This animal is an invertibrate.";
+    else {
+        document.getElementById("validator").innerHTML = "Incorrect!";
+    }
+
+
 }
 
-function displayHint2(){
-    let hint = document.getElementsByClassName("hide-hint-2");
-    for (let i=0; i <hint.length; i++){
-        hint[i].style.display = "inline";
+function displayHint(number) {
+    let hint = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < 3; i++) {
+        if (number == i) {
+            hint[i].style.display = "inline";
+            continue;
+        }
+        else {
+            hint[i].style.display = "none";
+        }
     }
-    document.getElementById("hint-2").innerHTML = "This animal is smaller than a loaf of bread.";
 }
 
-function displayHint3(){
-    let hint = document.getElementsByClassName("hide-hint-3");
-    for (let i=0; i <hint.length; i++){
-        hint[i].style.display = "inline";
+function nextButton() {
+    for (let i = 1; i < 4; i++) {
+        let hint = document.getElementsByClassName("tabcontent");
+        for (let i = 0; i < hint.length; i++) {
+            hint[i].style.display = "none";
+        }
     }
-    document.getElementById("hint-3").innerHTML ="This animal is slimy!";
+    fetch('./questionList.json')
+        .then(questions => questions.json())
+        .then(data => {
+            document.getElementById("question").innerHTML = data[questionNumber].funFact;
+            document.getElementById("hint-1").innerHTML = data[questionNumber].hints[0];
+            document.getElementById("hint-2").innerHTML = data[questionNumber].hints[1];
+            document.getElementById("hint-3").innerHTML = data[questionNumber].hints[2];
+
+            questionNumber++;
+            document.getElementById("validator").innerHTML = "";
+            document.getElementById("submit-button").style.display = "inline";
+            document.getElementById("answer-input").value = "";
+        });
+
 }
